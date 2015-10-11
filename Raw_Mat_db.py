0a872f77623f69319cf82cf35a98fe5f558f594f
Robert Creagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2015-10-11 10:30:19 +0100

    Add funtion to calculate distance

--- Raw_Mat_db.py
+++ Raw_Mat_db.py
@@ -7,6 +7,7 @@ Created on Sun Oct 11 10:23:05 2015
 
 import os
 import sqlite3
+import numpy as np
 import pandas as pd
 
 #Change read location to read database
@@ -27,3 +28,8 @@ df = pd.read_sql(sql, conn)
 
 #Read location table from database using Pandas
 df2 = pd.read_sql(sql2, conn)
+
+#Function to calculate distance between 2 points (x1, y1) and (x2, y2)
+def distance(x1,y1,x2,y2):
+    s = np.sqrt((pow(x2-x1,2)+pow(y2-y1,2)))
+    return s
